d347d4ba7ae3fac89782e51b57bd96fa0e0a82a4
SpaghettiAuChocolat <123958423+SpaghettiAuChocolat@users.noreply.github.com>
2024-02-15 11:12:52 -0500

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest 'https://i.kym-cdn.com/entries/icons/facebook/000/039/486/cover2.jpg' -OutFile "$Env:TEMP\skeleton.jpg"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name EnableTransparency -Value 0 -force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name Wallpaper -Value $ENV:TEMP"\skeleton.jpg" -force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value 2 -force
+New-ItemProperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Name LockScreenImage -Value $ENV:TEMP"\skeleton.jpg" -force
+Stop-Process -ProcessName explorer
