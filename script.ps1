b614fafc9654fd9f8c927855b9e36725f19a7417
SpaghettiAuChocolat <123958423+SpaghettiAuChocolat@users.noreply.github.com>
2024-02-26 17:22:23 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,6 @@
-Invoke-WebRequest 'https://i.kym-cdn.com/entries/icons/facebook/000/039/486/cover2.jpg' -OutFile "$Env:TEMP\skeleton.jpg"
+Invoke-WebRequest 'https://i.pinimg.com/originals/2c/d4/1f/2cd41f00f206636090a1061ff353accb.jpg' -OutFile "$Env:TEMP\skeleton.jpg"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name EnableTransparency -Value 0 -force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name Wallpaper -Value $ENV:TEMP"\skeleton.jpg" -force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value 2 -force
