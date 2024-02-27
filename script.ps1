4e0c86292b965149e0e2fd5039416cf2f83818cb
SpaghettiAuChocolat <123958423+SpaghettiAuChocolat@users.noreply.github.com>
2024-02-27 09:03:09 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
 Invoke-WebRequest 'https://i.pinimg.com/originals/2c/d4/1f/2cd41f00f206636090a1061ff353accb.jpg' -OutFile "$Env:TEMP\skeleton.jpg"
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -force
