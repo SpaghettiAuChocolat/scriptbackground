f50582e332ba83269f9ac7c81208b8fb692c3a3a
SpaghettiAuChocolat <123958423+SpaghettiAuChocolat@users.noreply.github.com>
2024-02-27 08:53:02 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,17 @@
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+  if ($elevated) {
+        # Privil�ges administrateurs non obtenus, annulation
+    }
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
 Invoke-WebRequest 'https://i.pinimg.com/originals/2c/d4/1f/2cd41f00f206636090a1061ff353accb.jpg' -OutFile "$Env:TEMP\skeleton.jpg"
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -force
