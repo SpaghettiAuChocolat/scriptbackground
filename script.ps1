03fb40b648274233d8795a852ad3a7688023fee9
SpaghettiAuChocolat <123958423+SpaghettiAuChocolat@users.noreply.github.com>
2024-08-30 09:45:23 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -8,27 +8,7 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\
 New-ItemProperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Name LockScreenImage -Value $ENV:TEMP"\image.jpg" -force
 Stop-Process -ProcessName explorer
 
-$adapters = Get-NetAdapter
-$adapterlist = @()
-foreach($adapter in $adapters){
-    $status =  $adapter | Select-Object Status
-    if($status.Status -eq "Up"){
-        $adapterlist += $adapter
-    }
-}
-$validint = @()
-foreach($interface in $adapterlist){
-    if($interface.Name.Substring(0,8) -contains "Ethernet" -and $interface.Name -notlike "*(CÉGEP)*"){
-        $validint += $interface
-    }
-}
-foreach($int in $validint){
-    $ipadd = Get-NetIPAddress | Where-Object InterfaceAlias -eq $int.Name
-    $ipv4 = $ipadd.IPv4Address
-    if ($ipv4 -notlike "169*"){
-        Disable-NetAdapter -Name $int.Name -Confirm:$false
-    }
-}
+
 
 
 #changes firefox version
